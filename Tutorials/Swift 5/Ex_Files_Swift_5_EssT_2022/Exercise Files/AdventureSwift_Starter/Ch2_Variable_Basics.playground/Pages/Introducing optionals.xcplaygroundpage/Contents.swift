2b0a4fcfc88da62fc522512e640c98b7b46c990e
pacs <pacs@outlook.pt>
2023-05-12 23:27:41 +0100

    almost done with chapter 2

--- Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch2_Variable_Basics.playground/Pages/Introducing optionals.xcplaygroundpage/Contents.swift
+++ Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch2_Variable_Basics.playground/Pages/Introducing optionals.xcplaygroundpage/Contents.swift
@@ -13,7 +13,9 @@
 
 */
 // Creating optionals
-
+var itemGathered: String? = "Pickaxe"
+var isExchangeable: Bool?
 
 // Forced unwrapping
-
+print(itemGathered!)
+//print(isExchangeable!)
--- Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch2_Variable_Basics.playground/Pages/Type conversions.xcplaygroundpage/Contents.swift
+++ Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch2_Variable_Basics.playground/Pages/Type conversions.xcplaygroundpage/Contents.swift
@@ -14,10 +14,11 @@
  
 */
 // Test variables
-
+var currentGold_Double = 5.832
 
 // Explicit conversions
-
-
+var currentGold_Integer: Int = Int(currentGold_Double)
+var currentGold_String = String(currentGold_Double)
 // Inferred conversion with operators
-
+var bankDeposit = 37 + 5.892
+var bankDeposit_Explic = currentGold_Double + Double(currentGold_Integer)
