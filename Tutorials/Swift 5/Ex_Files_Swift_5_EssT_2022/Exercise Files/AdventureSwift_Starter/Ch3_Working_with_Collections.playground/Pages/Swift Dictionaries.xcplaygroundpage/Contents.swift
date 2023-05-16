cd1434cb5d42dd68ead04c3fd598f3e9f92672cc
pacs <pacs@outlook.pt>
2023-05-16 22:46:06 +0100

    dictionaries introduction

--- Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch3_Working_with_Collections.playground/Pages/Swift Dictionaries.xcplaygroundpage/Contents.swift
+++ Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch3_Working_with_Collections.playground/Pages/Swift Dictionaries.xcplaygroundpage/Contents.swift
@@ -15,10 +15,16 @@
 
  */
 // Creating dictionaries
-
+var blacksmithShop: [String: Int] = ["Bottle": 10, "Shield": 15, "Ocarina": 1000]
 
 // Accessing and modifying values
+var shieldPrice = blacksmithShop["Shield"]
+blacksmithShop["Bottle"] = 11
 
-
+blacksmithShop["Towel"] = 1
+print(blacksmithShop)
 // All keys and values
+let allKeys = [String](blacksmithShop.keys)
+
+let allValues = [Int](blacksmithShop.values)
 
