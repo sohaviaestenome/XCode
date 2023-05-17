69ebb21554782ffc08163123119f4706a7fbb9b2
pacs <pacs@outlook.pt>
2023-05-17 10:12:23 +0100

    tuples

--- Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch3_Working_with_Collections.playground/Pages/Swift Tuples.xcplaygroundpage/Contents.swift
+++ Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch3_Working_with_Collections.playground/Pages/Swift Tuples.xcplaygroundpage/Contents.swift
@@ -17,12 +17,20 @@
 
  */
 // Simple tuple
+var uppercutAttack: (String, Int, Bool) = ("Uppercut Smash", 25, true)
+uppercutAttack.0
+uppercutAttack.1
+uppercutAttack.2
 
-
-
-
+var (attack, damage, rechargable) = uppercutAttack
+attack
+damage
+rechargable
 // Naming tuple values
 
+var planetSmash = (name: "Planet Smash", damage: 45, rechargable: true)
 
+planetSmash.rechargable
 // Naming values with type annotation
-
+var shieldStomp: (name: String, damage: Int, rechargeable: Bool)
+shieldStomp.damage = 100
