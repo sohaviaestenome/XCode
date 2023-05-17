c637b8f6b46e7540339a2bcd821b92e0003e8fc9
pacs <pacs@outlook.pt>
2023-05-17 23:40:23 +0100

    complex functions

--- Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch5_Wide_World_of_Functions.playground/Pages/Complex Functions.xcplaygroundpage/Contents.swift
+++ Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch5_Wide_World_of_Functions.playground/Pages/Complex Functions.xcplaygroundpage/Contents.swift
@@ -14,10 +14,30 @@
 
  */
 // Optional return value
+func setupArenaMatch() -> Bool? {
+    return nil
+}
 
-
+if let initSucess = setupArenaMatch()
+{
+    print("Level initilized: \(initSucess)")
+} else {
+    print("Something went terribly wrong...")
+}
 // Multiple return values
+func setupArenaMatch(levelName: String) -> (sucess: Bool, secretItem: String) {
+    print("\(levelName) initialized...")
+    return (true, "Minotaur's Head")
+}
 
+var levelDetails = setupArenaMatch(levelName: "Poison Flats")
+levelDetails.secretItem
+levelDetails.sucess
 
 // Default values
+func setupDefaultMatch(levelName: String = "Fire Marshes", opponents: Int = 3) {
+    print("Arena match will take place in the \(levelName) between \(opponents) players!")
+}
 
+setupDefaultMatch()
+setupDefaultMatch(levelName: "dreadzilla", opponents: 10)
