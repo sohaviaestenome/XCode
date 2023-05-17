836fb2da9300a40656e0fd0b40656c478209d4c5
pacs <pacs@outlook.pt>
2023-05-17 19:02:44 +0100

    overloading functions

--- Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch5_Wide_World_of_Functions.playground/Pages/Overloading Functions.xcplaygroundpage/Contents.swift
+++ Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch5_Wide_World_of_Functions.playground/Pages/Overloading Functions.xcplaygroundpage/Contents.swift
@@ -14,7 +14,22 @@
 
  */
 // Base function
-
+func attackEnemy() {
+    print("Executing default attack")
+}
 
 // Overloaded functions
+func attackEnemy(damage: Int) {
+    print("Attacking for \(damage)")
+}
 
+func attackEnemy(damage: Double, weapon: String) -> Bool {
+    let attackSuccess = true
+    print("Attacking enemy for \(damage) with \(weapon)")
+    return attackSuccess
+}
+
+
+attackEnemy()
+attackEnemy(damage: 34)
+var enemyDefeated = attackEnemy(damage: 32.6, weapon: "mace")
