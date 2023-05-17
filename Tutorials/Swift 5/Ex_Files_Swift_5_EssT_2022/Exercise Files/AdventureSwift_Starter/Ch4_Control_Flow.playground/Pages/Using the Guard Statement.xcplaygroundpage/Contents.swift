6dcbbb7f335fbe717026ec5418b3449c3755b20f
pacs <pacs@outlook.pt>
2023-05-17 18:30:05 +0100

    finished chapter 4

--- Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch4_Control_Flow.playground/Pages/Using the Guard Statement.xcplaygroundpage/Contents.swift
+++ Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch4_Control_Flow.playground/Pages/Using the Guard Statement.xcplaygroundpage/Contents.swift
@@ -22,7 +22,13 @@ let shopItems = ["Magic wand": 10, "Iron Helm": 5, "Excalibur": 1000]
 let currentGold = 16
 
 // Guard statement with for-in loop
-
+for (item, price) in shopItems {
+    guard currentGold >= price else {
+        print("You can't affor the \(item)")
+        continue
+    }
+    print("Go ahead, the \(item) if yours for \(price)")
+}
 
 // Guard check for specific item
 
