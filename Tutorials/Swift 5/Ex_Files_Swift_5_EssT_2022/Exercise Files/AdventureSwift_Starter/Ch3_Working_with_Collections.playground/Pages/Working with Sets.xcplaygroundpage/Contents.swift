7b0bad9a166b9ee3676dc8715c9e1956d8acef82
pacs <pacs@outlook.pt>
2023-05-17 09:50:37 +0100

    sets

--- Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch3_Working_with_Collections.playground/Pages/Working with Sets.xcplaygroundpage/Contents.swift
+++ Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch3_Working_with_Collections.playground/Pages/Working with Sets.xcplaygroundpage/Contents.swift
@@ -14,10 +14,13 @@
 
  */
 // Creating sets
-
+var activeQuests: Set = ["Fetch Gemstones", "Big Boss", "The Undertaker", "Granny needs Firewood"]
 
 // Inserting and removing elements
+activeQuests.insert("Only the Strong")
+activeQuests.remove("The Undertaker")
 
-
+print(activeQuests)
 // More common methods
-
+activeQuests.contains("All-4-One")
+activeQuests.sorted()
