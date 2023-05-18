542c0dd0987803ab0012006ea3c7df5211e6a025
pacs <pacs@outlook.pt>
2023-05-18 11:41:49 +0100

    structs

--- Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch6_Classes_Structs_and_Beyond.playground/Pages/Basic swift structs.xcplaygroundpage/Contents.swift
+++ Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/AdventureSwift_Starter/Ch6_Classes_Structs_and_Beyond.playground/Pages/Basic swift structs.xcplaygroundpage/Contents.swift
@@ -15,6 +15,37 @@
 
  */
 // Declaring a new struct
+struct Level {
+    let levelID: Int
+    var levelObjectives: [String]
+    
+    var levelDescription: String {
+        return "Level ID: \(self.levelID)"
+    }
+    
+    init(id: Int, objectives: [String]) {
+        self.levelID = id
+        self.levelObjectives = objectives
+    }
+    
+    func queryObjectives() {
+        for objective in levelObjectives {
+            print("\(objective)")
+        }
+    }
+    
+    mutating func completeObjective(index: Int) {
+        levelObjectives.remove(at: index)
+    }
+}
 
+var objectives = ["some crap", "another crap", "ohhhh boy"]
+var level1 = Level(id: 1, objectives: objectives)
 
 // Value types again
+var defaultLevel = level1
+
+level1.completeObjective(index: 0)
+level1.queryObjectives()
+
+defaultLevel.queryObjectives()
