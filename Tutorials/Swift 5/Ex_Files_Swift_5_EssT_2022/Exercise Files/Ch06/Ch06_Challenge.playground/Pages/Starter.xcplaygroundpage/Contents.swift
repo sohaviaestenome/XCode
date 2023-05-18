5df356f54c1616fd3e971579fa0254904afb2184
pacs <pacs@outlook.pt>
2023-05-18 12:19:35 +0100

    finished challenge chapter 6

--- Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/Ch06/Ch06_Challenge.playground/Pages/Starter.xcplaygroundpage/Contents.swift
+++ Tutorials/Swift 5/Ex_Files_Swift_5_EssT_2022/Exercise Files/Ch06/Ch06_Challenge.playground/Pages/Starter.xcplaygroundpage/Contents.swift
@@ -16,18 +16,49 @@
  
  */
 // 1
-
+class Item {
+    var name: String
+    var price: Int
+    var secret: BonusEffect?
+    
+    init(name: String, price: Int) {
+        self.name = name
+        self.price = price
+    }
+    
+}
 // 2
 
 // 3
-
+struct BonusEffect {
+    var bonus: Int
+    
+}
 // 4
 
 // 5
-
+class Inventory {
+    var storedItems: [Item]
+    
+    init(storedItems: [Item]){
+        self.storedItems = storedItems
+    }
+}
 // 6
 
 // 7
+var bonusInst = BonusEffect(bonus: 2)
+var item1 = Item(name: "Coisa", price: 3)
+var item2 = Item(name: "cena", price: 4)
+
+print(bonusInst.bonus)
+item2.secret = bonusInst
 
 // 8
+var invent1 = Inventory(storedItems: [item1, item2])
 
+if let itemPower = invent1.storedItems[1].secret?.bonus {
+    print("Item 2 has a bonus effect of \(itemPower)")
+} else {
+    print("item has no secret effect")
+}
