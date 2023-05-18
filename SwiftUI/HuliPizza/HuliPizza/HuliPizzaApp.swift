0bce728d1a51b3867879d2077d5abf403f4bb010
pacs <pacs@outlook.pt>
2023-05-18 19:24:13 +0100

    starting huli pizza

---
+++ SwiftUI/HuliPizza/HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Pedro Silva on 18/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
