5f3792f80c8e6eb08f5b872af1626fe84cc9c88a
pacs <pacs@outlook.pt>
2023-05-20 00:36:32 +0100

    favorites page

---
+++ SwiftUI/HuliPizza/HuliPizza/Views/FavoriteTileView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct FavoriteTileView: View {
+    var menuItem:MenuItem
+    var body: some View{
+        VStack{
+             Group{
+                if let image = UIImage(named:"\(menuItem.id)_sm"){
+                        Image(uiImage: image)
+                            .resizable()
+                            .scaledToFit()
+                } else {
+                    Image("surfboard_lg")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+             .clipShape(Capsule())
+             .shadow(radius: 3,x: 2,y: 2)
+            Text(menuItem.name).font(.caption2)
+                .padding(5)
+                .background(.regularMaterial)
+        }
+        
+       
+    }
+}
+
+struct FavoriteTileView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteTileView(menuItem: testMenuItem)
+    }
+}
